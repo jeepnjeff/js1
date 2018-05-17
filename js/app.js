6fc05d63cc5232fae3b80e00fc9065f7e581cfc7
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-05-16 19:10:08 -0700

    fix thur

--- js/app.js
+++ js/app.js
@@ -177,6 +177,53 @@ function addItemWed(listItem) {
   }
 }
 
+// *****************************Thursday***************************
+function addItemThur(listItem) {
+  console.log(day.value);
+  let listTime = document.getElementById('Thursday');
+  let listComment = document.createElement('li');
+  listComment.innerText = listItem;
+  listComment.classList.add('li-time');
+  listComment.classList.add('li-completed');
+  let listBox = document.createElement('div');
+  listBox.classList.add("listBox");
+  let remove = document.createElement("button");
+  remove.classList.add('remove');
+  remove.innerHTML = deleteIcon;
+  let complete = document.createElement("button");
+  complete.classList.add('complete');
+  complete.innerHTML = completeIcon;
+  listBox.appendChild(complete);
+  listBox.appendChild(remove);
+  listComment.appendChild(listBox);
+  listTime.appendChild(listComment);
+  // Delete Button Function
+  remove.addEventListener('click', deleteTodo);
+  function deleteTodo() {
+    let todoItem = listTime;
+    listTime.removeChild(listComment);}
+  // Complete Button Function
+  complete.addEventListener('click', completeTodo);
+  function completeTodo() {
+    let moveComplete = listBox.parentNode; //leave alone
+    let removeIcon = listBox;
+    let ulComplete = document.getElementById('ulComplete');
+    ulComplete.appendChild(moveComplete);
+    moveComplete.removeChild(removeIcon);
+    let remove = document.createElement("button");
+    remove.classList.add('remove-completed');
+    remove.innerHTML = deleteIcon;
+    moveComplete.appendChild(remove);
+    // Delete Button Function
+    remove.addEventListener('click', deleteTodo);
+    function deleteTodo() {
+      let finalRemove = moveComplete;
+      ulComplete.removeChild(moveComplete);
+    }
+  }
+}
+
+
 // *****************************Friday***************************
 function addItemFri(listItem) {
   console.log(day.value);
